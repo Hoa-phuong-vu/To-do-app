85e65cbd46cc3eaa2d481462addb16cbb0e5b462
Hoa-phuong-vu <hoavu060404@gmail.com>
2023-10-03 18:14:10 +1100

    added todos

--- todos.js
+++ todos.js
@@ -1,14 +1,36 @@
 let todos = [];
 
 const todosList = document.getElementById("todos"); 
+const todoInput = document.getElementById("textInput");
+const inputButton = document.getElementById("button");
+
+function addTodo(e) {
+    e.preventDefault()
+    let textValue = todoInput.value
+    todos.push(textValue)
+    todosList.innerHTML = "" //resets the list so it doesn't print the same todos multiple times
+    rendertodos()
+}
+
+inputButton.addEventListener("click", addTodo)
+
 function rendertodos() {
     todos.forEach((todo, i) => {
         let currentHTML = todosList.innerHTML;
         let newHTML = `
             <div class="todoItem">
-                <p>${i + 1}${todo}</p>
+                <p>${i + 1}. ${todo}</p>
+                <div class="actions">
+                    <i class="fa-solid fa-pen"></i>
+                    <i class="fa-solid fa-trash-can"></i>
+                </div>
             </div>
         `;
+
+        let amendedHTML = currentHTML + newHTML
+        todosList.innerHTML = amendedHTML
     })
 
 };
+
+rendertodos();
